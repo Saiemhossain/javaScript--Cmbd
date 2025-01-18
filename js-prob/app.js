2d3e691ffa875dc95081c7c9fd42423f91f8b71b
Saim <149803900+Saiemhossain@users.noreply.github.com>
2025-01-18 22:13:09 +0600

    Update app.js

--- js-prob/app.js
+++ js-prob/app.js
@@ -381,3 +381,19 @@ function randomNum(num) {
 }
 
 console.log(randomNum());
+
+// Check the number is prime or not
+
+function isPrime(num) {
+  if (num <= 1) {
+    return false
+  }
+  for (let i = 2; i <  num; i++){
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+
+console.log(isPrime(7));
